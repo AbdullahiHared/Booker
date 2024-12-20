6ac4ca57970d333f879d5600075c9de816aef0af
Abdullah Hared <muxsinxareed6@gmail.com>
2024-12-20 07:12:01 +0100

    Add Bus entity class with fields and getters/setters

---
+++ src/main/java/org/example/dao/Bus.java
@@ -0,0 +1,62 @@
+package org.example.dao;
+
+public class Bus {
+    private int id;
+    private String name;
+    private String route;
+    private int capacity;
+
+    // Constructor
+    public Bus(int id, String name, String route, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.route = route;
+        this.capacity = capacity;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Bus{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", route='" + route + '\'' +
+                ", capacity=" + capacity +
+                '}';
+    }
+
+
+}
