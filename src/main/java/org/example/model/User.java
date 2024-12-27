74e0c6b981852e6a99b52ac482663266839bf4e1
Abdullah Hared <muxsinxareed6@gmail.com>
2024-12-27 20:45:41 +0100

    update the user model

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -2,23 +2,25 @@ package org.example.model;
 
 public class User {
     private int id;
-    private String name;
     private String email;
+    private String name;
+    private String password; // Add password for authentication (optional)
 
-    // Constructor
-    public User(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    // Getter and Setter methods
+    // Getters and setters
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getName() {
@@ -29,11 +31,11 @@ public class User {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
+    public String getPassword() {
+        return password;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
