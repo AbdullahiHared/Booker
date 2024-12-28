9e0f1d959f2b76d6672d498cd66133a11058e557
Abdullah Hared <muxsinxareed6@gmail.com>
2024-12-28 14:17:35 +0100

    Test adding user

---
+++ src/main/java/org/example/test/UserDAOTest.java
@@ -0,0 +1,31 @@
+package org.example.test;
+
+import org.example.dao.UserDAO;
+import org.example.model.User;
+import org.example.database.DatabaseConnection;
+import org.example.database.SchemaInitializer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class UserDAOTest {
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            // Initialize the schema
+            SchemaInitializer.initializeSchema(connection);
+
+            // create userDAO instance
+            UserDAO userDAO = new UserDAO(connection);
+
+            // Test: add user
+            User user = new User();
+            user.setId(1);
+            user.setEmail("test1@example.com");
+            user.setName("test1");
+            user.setPassword("test1");
+
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
