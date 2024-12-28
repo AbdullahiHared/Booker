6aca9f60157d89eb2ee7da1f1fb17a986fed6237
Abdullah Hared <muxsinxareed6@gmail.com>
2024-12-28 13:27:22 +0100

    Added Users table

--- src/main/java/org/example/database/SchemaInitializer.java
+++ src/main/java/org/example/database/SchemaInitializer.java
@@ -11,19 +11,26 @@ public class SchemaInitializer {
                     name VARCHAR(255) NOT NULL,
                     route VARCHAR(255) NOT NULL,
                     capacity INT NOT NULL
-                )
+                );
                 
                 CREATE TABLE IF NOT EXISTS booking (
-                    id INT PRINTARY KEY AUTO_INCREMENT,
+                    id INT PRIMARY KEY AUTO_INCREMENT,
                     bus_id INT NOT NULL,
                     passenger_name VARCHAR(255) NOT NULL,
                     booking_date DATE NOT NULL,
                     FOREIGN KEY (bus_id) REFERENCES buses(id)
+                );
+                
+                CREATE TABLE IF NOT EXISTS users  (
+                    id INI PRIMARY KEY AUTO_INCREMENT,
+                    email VARCHAR(255) NOT NULL,
+                    name VARCHAR(255) NOT NULL,
+                    password VARCHAR(255) NOT NULL
                 )
-            """;
+                """;
 
         try (Statement statement = connection.createStatement()) {
-            statement.execute(createTableSQL);
+            statement.executeUpdate(createTableSQL); // Use executeUpdate for DDL statements.
             System.out.println("Database schema initialized successfully.");
         } catch (Exception e) {
             System.err.println("Error initializing database schema: " + e.getMessage());
