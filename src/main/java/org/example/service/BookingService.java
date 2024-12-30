1c0f95118fad5aa28a6d13a753d05dcaa88176cc
Abdullah Hared <muxsinxareed6@gmail.com>
2024-12-30 14:33:13 +0100

    Add logic for adding and updating booking

--- src/main/java/org/example/service/BookingService.java
+++ src/main/java/org/example/service/BookingService.java
@@ -1,4 +1,37 @@
 package org.example.service;
 
+
+import org.example.model.Booking;
+import org.example.dao.BookingDAO;
+import java.util.List;
+
 public class BookingService {
+    private BookingDAO bookingDAO;
+
+    public BookingService(BookingDAO bookingDao) {
+         this.bookingDAO = bookingDao;
+    }
+
+    // Add a booking
+    public boolean addBooking(Booking booking) {
+        // validation logic
+        if(booking.getPassengerName() == null || booking.getPassengerName().isEmpty()) {
+            System.out.println("Invalid passenger name. Booking not added.");
+            return false;
+        }
+        bookingDAO.addBooking(booking);
+        System.out.println("Booking added.");
+        return true;
+    }
+
+    // Update booking
+    public boolean updateBooking(Booking booking) {
+        if(booking.getPassengerName() == null || booking.getPassengerName().isEmpty()) {
+            System.out.println("Invalid passenger name. Booking not added.");
+            return false;
+        }
+        bookingDAO.updateBooking(booking);
+        System.out.println("Booking updated.");
+        return true;
+    }
 }
